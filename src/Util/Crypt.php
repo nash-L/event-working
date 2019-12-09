85845a1ba32b8050370fd873b9039fcd62021458
nash-L <18695616095@163.com>
2019-12-09 15:40:13 +0800

    基本数据

--- src/Util/Crypt.php
+++ src/Util/Crypt.php
@@ -23,8 +23,33 @@ class Crypt
         $this->publicKey = $this->rsa->getPublicKey();
     }
 
+    /**
+     * @return string
+     */
+    public function getPrivateKey(): string
+    {
+        return $this->privateKey;
+    }
+
+    /**
+     * @return bool|string
+     */
+    public function getPublicKey()
+    {
+        return $this->publicKey;
+    }
+
     public static function loadKey(string $privateKey): self
     {
         return new static($privateKey);
+    }
+
+    public static function create(): self
+    {
+        $rsa = new RSA();
+        $rsa->setPrivateKeyFormat(RSA::PRIVATE_FORMAT_PKCS1);
+        $rsa->setPublicKeyFormat(RSA::PUBLIC_FORMAT_PKCS1);
+        $keys = $rsa->createKey(1024);
+        return self::loadKey($keys['privatekey']);
     }
 }
--- src/Util/WorkSpace.php
+++ src/Util/WorkSpace.php
@@ -2,6 +2,8 @@
 namespace iWan\Util;
 
 use iWan\Throwable\DirectoryException;
+use Noodlehaus\Config;
+use Noodlehaus\Parser\Json;
 use Symfony\Component\Filesystem\Filesystem;
 
 class WorkSpace
@@ -10,10 +12,30 @@ class WorkSpace
      * @var Filesystem
      */
     private $filesystem;
+    /**
+     * @var string
+     */
     private $root_dir;
+    /**
+     * @var string
+     */
     private $runtime_dir = '/runtime';
-    private $tmp_dir = '/runtime/tmp';
+    /**
+     * @var string
+     */
     private $catch_dir = '/runtime/catch';
+    /**
+     * @var string
+     */
+    private $cert_dir = '/runtime/cert';
+    /**
+     * @var Config
+     */
+    private $config;
+    /**
+     * @var Crypt
+     */
+    private $crypt;
 
     /**
      * WorkSpace constructor.
@@ -27,15 +49,74 @@ class WorkSpace
         }
         $this->root_dir = realpath($workSpace);
         $this->filesystem = new Filesystem();
-        $this->initDir($this->runtime_dir);
-        $this->initDir($this->tmp_dir);
-        $this->initDir($this->catch_dir);
+        $this->init();
     }
 
-    private function initDir($dir)
+    /**
+     * @return Config
+     */
+    public function getConfig(): Config
     {
-        if (!$this->filesystem->exists($dir = $this->root_dir . $dir)) {
-            $this->filesystem->mkdir($dir);
+        return $this->config;
+    }
+
+    /**
+     * @return Crypt
+     */
+    public function getCrypt(): Crypt
+    {
+        return $this->crypt;
+    }
+
+    /**
+     *
+     */
+    private function init()
+    {
+        $this->initDirectory();
+        $this->initConfig();
+        $this->initCrypt();
+    }
+
+    /**
+     *
+     */
+    private function initDirectory()
+    {
+        if (!$this->filesystem->exists($runtime_dir = $this->root_dir . $this->runtime_dir)) {
+            $this->filesystem->mkdir($runtime_dir, 0755);
+        }
+        if (!$this->filesystem->exists($catch_dir = $this->root_dir . $this->catch_dir)) {
+            $this->filesystem->mkdir($catch_dir, 0755);
+        }
+        if (!$this->filesystem->exists($cert_dir = $this->root_dir . $this->cert_dir)) {
+            $this->filesystem->mkdir($cert_dir, 0755);
+        }
+    }
+
+    /**
+     *
+     */
+    private function initConfig()
+    {
+        $defaultConfig = [];
+        $this->config = Config::load(json_encode($defaultConfig), new Json, true);
+        if ($this->filesystem->exists($this->root_dir . '/config.json')) {
+            $this->config->merge(Config::load($this->root_dir . '/config.json', new Json));
+        }
+    }
+
+    /**
+     *
+     */
+    private function initCrypt()
+    {
+        if ($this->filesystem->exists($privateKeyPath = $this->root_dir . $this->cert_dir . '/private.pem')) {
+            $this->crypt = Crypt::loadKey(file_get_contents($privateKeyPath));
+        } else {
+            $this->crypt = Crypt::create();
+            $this->filesystem->dumpFile($this->root_dir . $this->cert_dir . '/private.pem', $this->crypt->getPrivateKey());
+            $this->filesystem->dumpFile($this->root_dir . $this->cert_dir . '/public.pem', $this->crypt->getPublicKey());
         }
     }
 }
